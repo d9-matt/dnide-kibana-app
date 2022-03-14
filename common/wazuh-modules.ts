e3f18abbe44d4fe9436d92a5126440f9f56daace
Matt Davis <mdavis@d9now.com>
2022-03-14 15:24:28 -0400

    rebranding to DNIDE

--- common/wazuh-modules.ts
+++ common/wazuh-modules.ts
@@ -120,7 +120,7 @@ export const WAZUH_MODULES = {
   },
   devTools: {
     title: 'API console',
-    description: 'Test the Wazuh API endpoints.'
+    description: 'Test the DNIDE API endpoints.'
   },
   logtest: {
     title: 'Test your logs',
