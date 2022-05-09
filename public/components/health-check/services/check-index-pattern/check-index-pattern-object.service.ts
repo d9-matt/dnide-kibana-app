05472a4608bcca8185a476c97f9f7b34389ab1c5
Cameron Helton <chelton@d9now.com>
2022-05-09 16:41:08 -0400

    wui/alert/manager back to wazuh

--- public/components/health-check/services/check-index-pattern/check-index-pattern-object.service.ts
+++ public/components/health-check/services/check-index-pattern/check-index-pattern-object.service.ts
@@ -35,7 +35,7 @@ export const checkIndexPatternObjectService =  async (appConfig, checkLogger: Ch
   checkLogger.info(`Found default index pattern with title [${defaultPatternId}]: ${indexPatternDefaultFound ? 'yes' : 'no'}`);
 
   if (!indexPatternDefaultFound && defaultPatternId) {
-    // if no valid index patterns are found we try to create the portal9-alerts-*
+    // if no valid index patterns are found we try to create the wazuh-alerts-*
     try {
       checkLogger.info(`Checking if index pattern [${defaultPatternId}] exists...`);
       const existDefaultIndexPattern = await SavedObject.getExistingIndexPattern(defaultPatternId);
